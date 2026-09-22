6a471a565506183ed8bc590d789b506c737290a6
OlexandrYar <olexandryarovyi@gmail.com>
2026-09-22 14:11:52 +0100

    added displayList method and use it in tests

--- src/Test.java
+++ src/Test.java
@@ -1,31 +1,42 @@
 public class Test {
+    public static void displayList(ListArrayBased list){
+        System.out.println("Contents of the list:");
+        System.out.println("#============#");
+        for (int i = 1; i<=list.size(); i++){
+            System.out.printf("%d %2s\n",i,list.get(i));
+        }
+        System.out.println("#============#");
+    }
     public static void main(String[] args){
         ListArrayBased testList = new ListArrayBased();
         //check if the list is empty
+        System.out.println("Test 1");
         System.out.println("Is the list empty: " + testList.isEmpty());
         System.out.println("Size of the list: " + testList.size());
+        displayList(testList);
         //adding and getting the item test
         testList.add(1, "Dog");
         testList.add(2, "Cat");
         testList.add(3, "Parrot");
-        System.out.println("Item at index 1 is: " + testList.get(1));
-        System.out.println("Item at index 2 is: " + testList.get(2));
-        System.out.println("Item at index 3 is: " + testList.get(3));
+        System.out.println("Test 2");
+        displayList(testList);
         //double check
         System.out.println();
         System.out.println("Is the list empty: " + testList.isEmpty());
         System.out.println("Size of the list: " + testList.size());
+
         //Remove and removeAll methods test
-        System.out.println();
+        System.out.println("Test 3");
         testList.remove(2);
-        System.out.println(testList.get(2));//now is parrot as the list has shifted
+        displayList(testList);
         System.out.println("Size of the list: " + testList.size());
+        System.out.println("List after removeAll method");
         testList.removeAll();
-        System.out.println();
         System.out.println("Size of the list: " + testList.size());
 
         //Exception tests
-        testList.add(51,"Rabbit");
+        //testList.add(51,"Rabbit");
+        //testList.remove(-12);
 
     }
 }
